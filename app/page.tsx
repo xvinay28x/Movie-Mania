e01b1d1385cde160efd551cce241550bef52da7a
xvinay28x <xvinay28x@gmail.com>
2024-02-07 19:49:17 +0530

    hotfix missing keys

--- app/page.tsx
+++ app/page.tsx
@@ -1,40 +1,38 @@
-'use client'
+"use client";
 
 import { useEffect, useState } from "react";
 import NavBar from "./navBar";
 import { fetchMovies } from "./store";
-import { MovieResult } from './type'
-import {VerticalMovieCard, HorizontalMovieCard} from "./card";
-
-
+import { MovieResult } from "./type";
+import { VerticalMovieCard, HorizontalMovieCard } from "./card";
 
 export default function Home() {
-
-  const [moviesInfo, setMoviesInfo] = useState<MovieResult>()
+  const [moviesInfo, setMoviesInfo] = useState<MovieResult>();
 
   useEffect(() => {
-    fetchMovies()
-    .then((res) => {
-      setMoviesInfo(res)
-      
-    })
-  },[])
+    fetchMovies().then((res) => {
+      setMoviesInfo(res);
+    });
+  }, []);
 
   return (
     <>
-    <div className="h-screen w-screen bg-stone-950">
-      <NavBar/>
-      <div className="grid grid-flow-col scrollbar-hide gap-4 items-center w-full overflow-x-scroll py-12">
-      {moviesInfo?.results.map((movie) => (
-        <HorizontalMovieCard movie_img={movie.backdrop_path} />
-      ))}
+      <div className="h-screen w-screen bg-stone-950">
+        <NavBar />
+        <div className="grid grid-flow-col scrollbar-hide gap-4 items-center w-full overflow-x-scroll py-12">
+          {moviesInfo?.results.map((movie) => (
+            <HorizontalMovieCard
+              key={movie.id}
+              movie_img={movie.backdrop_path}
+            />
+          ))}
+        </div>
+        <div className="grid grid-flow-col scrollbar-hide gap-4 items-center w-full overflow-x-scroll py-12">
+          {moviesInfo?.results.map((movie) => (
+            <VerticalMovieCard key={movie.id} movie_img={movie.poster_path} />
+          ))}
+        </div>
       </div>
-      <div className="grid grid-flow-col scrollbar-hide gap-4 items-center w-full overflow-x-scroll py-12">
-      {moviesInfo?.results.map((movie) => (
-        <VerticalMovieCard movie_img={movie.poster_path} />
-      ))}
-      </div>
-    </div>
     </>
   );
 }
